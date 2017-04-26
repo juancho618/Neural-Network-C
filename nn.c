18a75bb13882b1c4a963c3f72afab8e6dc743975
Juan <jjsorianoe@gmail.com>
2017-04-26 17:59:59 +0200

    final changes 26

--- nn.c
+++ nn.c
@@ -1,5 +1,11 @@
 #include "nn.h"
 
+double nn_act_sigmoid(double a) {
+    if (a < -45.0) return 0; // ?
+    if (a > 45.0) return 1;
+    return 1.0 / (1 + exp(-a));
+}
+
 nn *nn_init(int inputs, int hidden_layers, int hidden, int outputs) {
 
     /* parameters valitation */
@@ -10,10 +16,44 @@ nn *nn_init(int inputs, int hidden_layers, int hidden, int outputs) {
 
     const int hidden_weights = hidden_layers ? (inputs+1) * hidden + (hidden_layers-1) * (hidden+1) * hidden : 0; // connetion to the hidden layer + nodes in the hidden
     const int output_weights = (hidden_layers ? (hidden+1) : (inputs+1)) * outputs; // number of outputs connection from hidden layer + output nodes
-    const int total_weights = (hidden_weights + output_weights
+    const int total_weights = (hidden_weights + output_weights);
 
     const int total_neurons = (inputs + hidden * hidden_layers + outputs);
 
+    /* Allocate extra size for weights, outputs, and deltas. */
+    const int size = sizeof(nn) + sizeof(double) * (total_weights + total_neurons + (total_neurons - inputs));
+    nn *ret = malloc(size);
+    if (!ret) return 0;
+
+    ret->inputs = inputs;
+    ret->hidden_layers = hidden_layers;
+    ret->hidden = hidden;
+    ret->outputs = outputs;
+
+    ret->total_weights = total_weights;
+    ret->total_neurons = total_neurons;
+
+    /* Set pointers. */
+    ret->weight = (double*)((char*)ret + sizeof(nn));
+    ret->output = ret->weight + ret->total_weights;
+    ret->delta = ret->output + ret->total_neurons;
+
+    nn_randomize(ret);
+
+    ret->activation_hidden = nn_act_sigmoid;
+    ret->activation_output = nn_act_sigmoid;
+
+    return ret;
 
 
+
+}
+
+void nn_randomize(nn *ann) {
+    int i;
+    for (i = 0; i < ann->total_weights; ++i) {
+        double r = NN_RANDOM();
+        /* Sets weights from -0.5 to 0.5. */
+        ann->weight[i] = r - 0.5;
+    }
 }
--- nn.h
+++ nn.h
@@ -1,7 +1,36 @@
 
+#include <stdio.h>
+
+#ifndef NN_RANDOM
+/* We use the following for uniform random numbers between 0 and 1.
+ * If you have a better function, redefine this macro. */
+#define NN_RANDOM() (((double)rand())/RAND_MAX)
+#endif
+
 typedef struct nn {
-  
+
     /* How many inputs, outputs, and hidden neurons. */
-    int inputs, hidden_layers, hidden, outputs;
+    int inputs, hidden_layers, hidden, outputs
+
+    /* Which activation function to use for hidden neurons. Default: nn_act_sigmoid */
+    nn_actfun activation_hidden;
+
+    /* Which activation function to use for output. Default: nn_act_sigmoid */
+    nn_actfun activation_output;
+
+    /* Total number of weights, and size of weights buffer. */
+    int total_weights;
+
+    /* Total number of neurons + inputs and size of output buffer. */
+    int total_neurons;
+
+    /* All weights (total_weights long). */
+    double *weight;
+
+    /* Stores input array and output of each neuron (total_neurons long). */
+    double *output;
+
+    /* Stores delta of each hidden and output neuron (total_neurons - inputs long). */
+    double *delta;
 
   } nn;
